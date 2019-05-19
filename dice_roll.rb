ca7fbb992cbb7e57e76ada2a898bbe2755317a72
Federica Macchi <federica.macchi3@gmail.com>
2019-05-19 17:42:47 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -1,15 +1,15 @@
-# Create method `roll` that returns a random number between 1 and 6
+# # Create method `roll` that returns a random number between 1 and 6
 
-# Feel free to google "how to generate a random number in ruby"
+# # Feel free to google "how to generate a random number in ruby"
 
-def roll
-  return (rand(1..6))
-end
+# def roll
+#   return (rand(1..6))
+# end
 
 # array = [1, 2, 3, 4, 5, 6]
 
-# def roll(array)
-#   random = array.sample
-#   return random
+def roll
+  random = array.sample
+  return random
 
-# end
+end
